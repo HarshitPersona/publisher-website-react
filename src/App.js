65277e9836a03af482c4dd1f36ac23e7be903778
Harshit Kumar <harshitkumar@Harshits-MacBook-Pro.local>
2023-05-12 13:31:35 +0530

    Integrated axios and sdk

--- src/App.js
+++ src/App.js
@@ -1,24 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+
+import axios from "axios";
+import { PersonaSDK } from "ad-sdk-practice";
 
 function App() {
+  const config = {
+    apiKey: "c2f376f0-4077-40e5-ad64-c26d80a879a9",
+    appId: "645ce04c44984e299373da14",
+  };
+
+  const generateApiKeyButtonEl = document.getElementById(
+    "generateApiKeyButton"
+  );
+  const createAssetButtonEl = document.getElementById("createAssetButton");
+
+  const callTheApi = async () => {
+    const adSdk = new PersonaSDK(config);
+    await adSdk.initialize();
+    adSdk.showAd(
+      "dummyplacementid",
+      "banner",
+      { dimensions: { width: 600, height: 160 } },
+      "banner-1"
+    );
+    adSdk.showAd(
+      "dummyplacementid",
+      "banner",
+      { dimensions: { width: 300, height: 250 } },
+      "banner-2"
+    );
+    adSdk.showAd(
+      "dummyplacementid",
+      "banner",
+      { dimensions: { width: 600, height: 160 } },
+      "banner-3"
+    );
+  };
+
+  generateApiKeyButtonEl?.addEventListener("click", async () => {
+    const res = await axios.patch(
+      "http://localhost:3000/api/publishers/api-key",
+      { email: "harshit1kumar@gmail.com" }
+    );
+    console.log(res);
+  });
+
+  createAssetButtonEl?.addEventListener("click", async () => {
+    const res = await axios.post("http://localhost:3000/api/assets", {
+      email: "harshit1kumar@gmail.com",
+    });
+    console.log(res);
+  });
+
+  useEffect(() => {
+    callTheApi();
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>Welcome to My Gaming Website</h1>
+        <div id="banner-1"></div>
       </header>
-    </div>
+      <nav>
+        <ul>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/games">Games</a>
+          </li>
+          <li>
+            <a href="/reviews">Reviews</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <section>
+        <h2>Featured Games</h2>
+        <div id="banner-2"></div>
+        <div className="game">
+          <h3>Game 1</h3>
+          <p>Description of Game 1.</p>
+        </div>
+        <div id="banner-3"></div>
+        <div className="game">
+          <h3>Game 2</h3>
+          <p>Description of Game 2.</p>
+        </div>
+      </section>
+      <footer>&copy; 2023 My Gaming Website. All rights reserved.</footer>
+    </>
   );
 }
 
